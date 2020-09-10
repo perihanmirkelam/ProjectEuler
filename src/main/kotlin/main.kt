01617cf0c5577c4008fcda584e770006aee27785
Perihan Mirkelam <perihan.mirkelam@ardictech.com>
2020-09-10 18:39:05 +0300

    Q9-Special Pythagorean triplet

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,6 +9,7 @@ fun main(args: Array<String>) {
     q6()
     q7()
     q8()
+    q9()
 }
 
 /**
@@ -190,4 +191,27 @@ fun q8(){
         product = 1
     }
     println("A8: $sup")
+}
+
+/**
+ * Q9-Special Pythagorean triplet
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * a^2 + b^2 = c^2
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+fun q9(){
+    val sum = 1000
+    var a: Int; var b: Int; var c: Int
+    var bPlusC: Int
+    for (i in sum/3..sum-2){ //triangle inequality
+        a = i
+        bPlusC = sum - i
+        for(j in 1..bPlusC/2){
+            b = j
+            c = bPlusC -b
+            if((b*b) + (c*c) == (a*a)) println("A9: ${a*b*c}")
+        }
+    }
 }

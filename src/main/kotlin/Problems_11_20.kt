11e64511d7de0d9fee2f9d0260227ddb257f25d6
Perihan Mirkelam <perihan.mirkelam@ardictech.com>
2020-09-12 16:20:38 +0300

    Q14-Longest Collatz sequence

--- src/main/kotlin/Problems_11_20.kt
+++ src/main/kotlin/Problems_11_20.kt
@@ -207,6 +207,37 @@ fun q13() {
             "20849603980134001723930671666823555245252804609722" +
             "53503534226472524250874054075591789781264330331690"
 
-    val sum = str.chunked(50).map{it.toBigInteger()}.reduce{ sum, it -> sum + it}
-    println("A13: ${sum.toString().substring(0,10)}")
+    val sum = str.chunked(50).map { it.toBigInteger() }.reduce { sum, it -> sum + it }
+    println("A13: ${sum.toString().substring(0, 10)}")
+}
+
+/**
+ * Q14-Longest Collatz sequence
+ * The following iterative sequence is defined for the set of positive integers:
+ *      n → n/2 (n is even)
+ *      n → 3n + 1 (n is odd)
+ * Using the rule above and starting with 13, we generate the following sequence:
+ *      13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+ * It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+ * Which starting number, under one million, produces the longest chain?
+ * NOTE: Once the chain starts the terms are allowed to go above one million.
+ */
+fun q14() {
+    var term: Long
+    var termCount: Int
+    var greatestTermCount = 0
+    var greatest = 0L
+    repeat(1_000_000) {
+        termCount = 1
+        term = it.toLong()
+        while (term > 1) {
+            term = if (term % 2 == 0L) term / 2 else 3 * term + 1
+            termCount++
+        }
+        if(termCount > greatestTermCount) {
+            greatest = it.toLong()
+            greatestTermCount = termCount
+        }
+    }
+    println("A14: $greatest")
 }
